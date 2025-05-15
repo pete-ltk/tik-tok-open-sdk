d3318fdd261a846f76397fd5451dc2288e0c19b1
Peter Schuette <peter.schuette@rewardstyle.com>
2025-05-15 10:45:24 -0500

    adds binary framework target

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TikTokOpenSDK",
+    platforms: [.iOS(.v16)],
+    products: [
+        .library(
+            name: "TikTokOpenSDK",
+            targets: ["TikTokOpenSDK"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(name: "TikTokOpenSDK", path: "Frameworks/TikTokOpenSDK.xcframework")
+    ]
+)
